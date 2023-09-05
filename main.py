13a1690ffa1195b71ad9985ae1852e8b73062361
nekitalek <nekitness@gmail.com>
2023-09-05 13:10:47 +0300

    fix

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import time
+import undetected_chromedriver as uc
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import undetected_chromedriver as uc
 
-driver = uc.Chrome()
+
+driver=uc.Chrome()
 driver.get('https://www.tiktok.com/foryou?lang=ru')
-time.sleep(10)
-
+delay = 5
+time.sleep(delay)
 webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-time.sleep(10)
+time.sleep(delay)
 
 element = driver.find_element(By.XPATH, '//*[@id="main-content-homepage_hot"]/div[1]/div[1]/div/div[1]/div[1]/a[2]')
 element.click()
-time.sleep(10)
+time.sleep(delay)
